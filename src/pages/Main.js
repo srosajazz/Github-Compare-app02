831fbc09fe12a2d2183c460403c130b7d9843f65
Sergio Rosa <scarlosjazz@gmail.com>
2019-05-02 13:20:03 -0400

    Stylize logo and form

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,56 @@
 import React from "react";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
 
-const Main = () => <h1>Hello World!</h1>;
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 60px;
+`;
+
+const Form = styled.form`
+    margin-top: 20px;
+    width: 100%;
+    max-width: 400px;
+    display: flex;
+
+    input {
+        flex: 1;
+        height: 55px;
+        padding: 0 20px;
+        background: orangered;
+        border: #444;
+        border-radius: 3px;
+    }
+
+    button {
+        height: 55px;
+        padding: 0 20px;
+        margin-left: 10px;
+        background: #63f5b6;
+        border: 0;
+        font-size: 20px;
+        font-weight: bold;
+        border-radius: 3px;
+
+        &:hover {
+            background: #52d89f;
+        }
+    }
+`;
+
+const Main = () => (
+    <div>
+        <img src={logo} alt="Github Compare" />
+
+        <Container>
+            <Form>
+                <input type="text" placeholder="user/repository" />
+                <button type="submit">OK</button>
+            </Form>
+        </Container>
+    </div>
+);
 
 export default Main;
