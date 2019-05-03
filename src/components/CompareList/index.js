314b003bc0aa89455cf5f53f0bbfbb234cd596bd
Sergio Rosa <scarlosjazz@gmail.com>
2019-05-03 08:22:20 -0400

    Sending repositories with props

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -2,103 +2,41 @@ import React from "react";
 
 import { Container, Repository } from "./style";
 
-const CompareList = () => (
+const CompareList = ({ repositories }) => (
     <Container>
-        <Repository>
-            <header>
-                <img
-                    src="https://avatars3.githubusercontent.com/u/69631?v=4"
-                    alt="facebook"
-                />
-                <strong>react</strong>
-                <small>facebook</small>
-            </header>
+        {repositories.map(repository => (
+            <Repository>
+                <header>
+                    <img
+                        src={repository.owner.avatar_url}
+                        alt={repository.owner.login}
+                    />
+                    <strong>{repository.name}</strong>
+                    <small>{repository.owner.login}</small>
+                </header>
 
-            <ul>
-                <li>
-                    96,019 <small>stars</small>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    96,019 <small>forks</small>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    96,019 <small>issues</small>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    3 days ago <small>last commit</small>
-                </li>
-            </ul>
-        </Repository>
-        {/* 2 */}
-        <Repository>
-            <header>
-                <img
-                    src="https://avatars3.githubusercontent.com/u/69631?v=4"
-                    alt="facebook"
-                />
-                <strong>react</strong>
-                <small>facebook</small>
-            </header>
-
-            <ul>
-                <li>
-                    96,019 <small>stars</small>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    96,019 <small>forks</small>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    96,019 <small>issues</small>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    3 days ago <small>last commit</small>
-                </li>
-            </ul>
-        </Repository>
-        {/* 3 */}
-        <Repository>
-            <header>
-                <img
-                    src="https://avatars3.githubusercontent.com/u/69631?v=4"
-                    alt="facebook"
-                />
-                <strong>react</strong>
-                <small>facebook</small>
-            </header>
-
-            <ul>
-                <li>
-                    96,019 <small>stars</small>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    96,019 <small>forks</small>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    96,019 <small>issues</small>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    3 days ago <small>last commit</small>
-                </li>
-            </ul>
-        </Repository>
+                <ul>
+                    <li>
+                        {repository.stargazers_count} <small>stars</small>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        {repository.forks_count} <small>forks</small>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        {repository.open_issues_count} <small>issues</small>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        {repository.pushed_at} <small>last commit</small>
+                    </li>
+                </ul>
+            </Repository>
+        ))}
     </Container>
 );
 
